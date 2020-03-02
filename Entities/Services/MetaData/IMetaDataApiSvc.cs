a89f6cdb5a502ac559b22895a39f8b4f6751e52e
jpsietsma <jpsietsma@gmail.com>
2020-03-02 11:12:29 -0500

    Upgraded .net core v3.0 -> 3.1 in Entities, TelevisionToolset, LibraryToolset.

---
+++ Entities/Services/MetaData/IMetaDataApiSvc.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Data.TmDB;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Entities.Services.MetaData
+{
+    public interface IMetaDataApiSvc
+    {
+        string RequestUrl { get; }
+
+        Task<T1> GetShowResultAsync<T, T1>(int ImdbId) 
+            where T : class
+            where T1: IApiCallResult;
+        Task<T1> GetShowResultAsync<T, T1>(params ObjectParameter[] _parameters)
+            where T : class
+            where T1: IApiCallResult;
+        Task<T1> GetShowResultAsync<T, T1>(string showName)
+            where T : class
+            where T1: IApiCallResult;
+
+        Task<List<T1>> GetManyShowResultsAsync<T, T1>(string showName)
+           where T : IApiCallMultipleResultset
+           where T1 : IApiCallResult;
+
+        Task<TheMovieDbExternalIds> GetExternalIds(int theMovieDbId);
+        
+        dynamic GetResult();
+    }
+}
