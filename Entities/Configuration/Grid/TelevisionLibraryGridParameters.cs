6c52d749e1d419ec8150277152d77fad8c55e505
jpsietsma <jpsietsma@gmail.com>
2020-03-24 14:54:32 -0400

    Added QueryStringURLParameters abstract class for using url parameters.  Moved paging logic to abstract class.  TelevisionLibraryGridParameters class is empty but inherits from the abstract.

--- Entities/Configuration/Grid/TelevisionLibraryGridParameters.cs
+++ Entities/Configuration/Grid/TelevisionLibraryGridParameters.cs
@@ -1,25 +1,12 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Configuration.Grid
 {
-    public class TelevisionLibraryGridParameters
+    public class TelevisionLibraryGridParameters : QueryStringURLParameters
     {
-		const int maxPageSize = 15;
-		public int PageNumber { get; set; } = 1;
-
-		private int _pageSize = 15;
-		public int PageSize
-		{
-			get
-			{
-				return _pageSize;
-			}
-			set
-			{
-				_pageSize = (value > maxPageSize) ? maxPageSize : value;
-			}
-		}
+		
 	}
 }
